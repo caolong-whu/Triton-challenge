d3c333cef8313acfae9c8f8a35c941f04181f4e5
Cao Long <117433408+caolong-whu@users.noreply.github.com>
2025-11-19 22:20:02 +0800

    finished

--- day02/layernorm.py
+++ day02/layernorm.py
@@ -25,23 +25,23 @@ def _layer_norm_fwd_fused(
 
     # compute mean
     mean = 0
-    _mean = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    sum_block = tl.float32(0.0)
     for off in tl.arange(0, N, BLOCK_SIZE):
         # [0, 1, 2, ..., BLOCK_SIZE-1]
         cols = off + tl.arange(0, BLOCK_SIZE)
         a = tl.load(X + cols, mask=cols < N, other=0.).to(tl.float32)
-        _mean += a
-    mean = tl.sum(_mean, axis=0) / N
+        sum_block += tl.sum(a, axis=0)
+    mean = sum_block / N
 
     # compute variance 
-    _var = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    sum_sq = tl.float32(0.0)
     for off in tl.arange(0, N, BLOCK_SIZE):
         cols = off + tl.arange(0, BLOCK_SIZE)
-        x = tl.load(X + off, mask=cols < N, other=0.).to(tl.float32)
+        x = tl.load(X + cols, mask=cols < N, other=0.).to(tl.float32)
         x = tl.where(cols < N, x - mean, 0.)
-        _var += x * x
-    var = tl.sum(_var, axis=0) / N
-    rstd = 1 / var
+        sum_sq += tl.sum(x * x, axis=0)
+    var = sum_sq / N
+    rstd = 1 / tl.math.sqrt(var + eps)
     # write mean and rstd
     tl.store(Mean + pid_row, mean)
     tl.store(Rstd + pid_row, rstd)
@@ -57,4 +57,6 @@ def _layer_norm_fwd_fused(
         y = w * x_hat + b
         # write ouput
         tl.store(Y + cols, y, mask=mask)
+
+
 
