90308c8c180a4969c12d5d767eaed71a2b777ff3
Cao Long <117433408+caolong-whu@users.noreply.github.com>
2025-11-19 22:07:04 +0800

    day02

---
+++ day02/layernorm.py
@@ -0,0 +1,60 @@
+import torch
+import triton
+import triton.language as tl
+
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+@triton.jit
+def _layer_norm_fwd_fused(
+    x, # pointer to the input
+    Y, # pointer to the output
+    W, # pointer to the weights
+    B, # pointer to the bias
+    Mean, # pointer to the mean
+    Rstd, # pointer to the 1/std
+    stride, # stride of the row
+    N, # number of columns in X
+    eps,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid_row = tl.program_id(0)
+    
+    X += pid_row * stride
+    Y += pid_row * stride
+
+    # compute mean
+    mean = 0
+    _mean = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    for off in tl.arange(0, N, BLOCK_SIZE):
+        # [0, 1, 2, ..., BLOCK_SIZE-1]
+        cols = off + tl.arange(0, BLOCK_SIZE)
+        a = tl.load(X + cols, mask=cols < N, other=0.).to(tl.float32)
+        _mean += a
+    mean = tl.sum(_mean, axis=0) / N
+
+    # compute variance 
+    _var = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    for off in tl.arange(0, N, BLOCK_SIZE):
+        cols = off + tl.arange(0, BLOCK_SIZE)
+        x = tl.load(X + off, mask=cols < N, other=0.).to(tl.float32)
+        x = tl.where(cols < N, x - mean, 0.)
+        _var += x * x
+    var = tl.sum(_var, axis=0) / N
+    rstd = 1 / var
+    # write mean and rstd
+    tl.store(Mean + pid_row, mean)
+    tl.store(Rstd + pid_row, rstd)
+
+    # layernorm
+    for off in tl.arange(0, N, BLOCK_SIZE):
+        cols = off + tl.arange(0, BLOCK_SIZE)
+        mask = cols < N
+        w = tl.load(W + cols, mask=mask)
+        b = tl.load(B + cols, mask=mask)
+        x = tl.load(X + cols, mask=mask, other=0.).to(tl.float32)
+        x_hat = (x - mean) * rstd
+        y = w * x_hat + b
+        # write ouput
+        tl.store(Y + cols, y, mask=mask)
+
